6ffa851711fede1df04bc4d24581d4f2fd124330
Ákos <akoshervay@gmail.com>
2017-05-21 23:42:27 +0200

    moved errors dict to separate file

---
+++ src/SignUp/errors.ts
@@ -0,0 +1,8 @@
+
+
+export const errorsDict: any = {
+  'GraphQL error: Could not find a user with that username': {
+    username: 'User does not exist', _error: 'Login failed!'
+  },
+  'GraphQL error: Invalid password.': { password: 'Invalid Password', _error: 'Login failed!' }
+};
